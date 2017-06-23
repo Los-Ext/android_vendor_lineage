33b3075a462572175c48b1c42d4308b251ebac90
Mathieu Chartier <mathieuc@google.com>
2017-06-23 14:59:56 -0700

    config: Disable dexpreopt debug info
    Test: lunch statix_cannon-user && m
    Change-Id: I9e4b4c9907b92fea764cf07e218541bb33937b3c
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/13530

--- config/common.mk
+++ config/common.mk
@@ -128,6 +128,9 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 # Disable dex2oat debug
 USE_DEX2OAT_DEBUG := false
 
+# Disable dexpreopt debug info
+WITH_DEXPREOPT_DEBUG_INFO := false
+
 # Enable whole-program R8 Java optimizations for SystemUI and system_server,
 # but also allow explicit overriding for testing and development.
 SYSTEM_OPTIMIZE_JAVA ?= true
