0ef814d6f4aeeac13ae167e3518346099eaab8e2
Danny Lin <danny@kdrag0n.dev>
2020-11-06 21:56:45 -0800

    config: Disable RescueParty
    RescueParty's aggressive approach to error recovery almost never helps
    with the problems we're likely to face, and its risk of data loss is far
    too high. Force-disable it entirely and remove all other conditions,
    especially those that depend on data integrity in the first place, to
    reduce the risk of RescueParty triggering falsely and causing data loss.
    
    Note that it is still possible to force-enable RescueParty for testing
    purposes using the persist.sys.enable_rescue property.

--- config/common_ext.mk
+++ config/common_ext.mk
@@ -6,6 +6,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.graphics.game_default_frame_rate.disabled=true
 
+# Disable RescueParty due to high risk of data loss
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.disable_rescue=true
+
 # Disable touch video heatmap to reduce latency, motion jitter, and CPU usage
 # on supported devices with Deep Press input classifier HALs and models
 PRODUCT_PRODUCT_PROPERTIES += \
