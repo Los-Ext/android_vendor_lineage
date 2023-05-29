3ec5bed64a6c5750f7fac8e3144460771fec638f
NurKeinNeid <mralexman3000@gmail.com>
2023-05-29 13:41:21 +0200

    config: Introduce Accord Music player
    Change-Id: Ia95ec6115c2b971e6a2fce729bf6958a3701418d
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- config/common.mk
+++ config/common.mk
@@ -364,6 +364,7 @@ $(call inherit-product-if-exists, vendor/google/gms/config.mk)
 $(call inherit-product-if-exists, vendor/google/pixel/config.mk)
 else
 PRODUCT_PACKAGES += \
+    Accord \
     ExactCalculator \
     Jelly \
     Etar \
