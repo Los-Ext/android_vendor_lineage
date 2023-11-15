06af203a2afc246f7f8d7b4728913280ad3f4108
ponces <ponces26@gmail.com>
2023-11-15 16:02:38 +0000

    lineage: Switch to AOSPA's face unlock implementation
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -68,3 +68,14 @@ PRODUCT_PACKAGES += \
     LineageBlackTheme \
     ThemePicker \
     ThemesStub
+
+ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
+PRODUCT_PACKAGES += \
+    FaceUnlock
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
