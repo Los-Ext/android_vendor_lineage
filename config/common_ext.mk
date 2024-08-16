b556cc65cf00e4c16954f38d7e0aed42f8d97692
minaripenguin <minaripenguin@users.noreply.github.com>
2024-08-17 07:58:33 +0800

    config: Disable memtag for different processes
    * it defaults to async if not specified
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/common_ext.mk
+++ config/common_ext.mk
@@ -10,3 +10,11 @@ PRODUCT_PACKAGES += \
     Etar \
     LineageSetupWizard
 endif
+
+# Memtag
+PRODUCT_PRODUCT_PROPERTIES += \
+    arm64.memtag.process.system_server=off \
+    persist.arm64.memtag.app.com.android.se=off \
+    persist.arm64.memtag.app.com.google.android.bluetooth=off \
+    persist.arm64.memtag.app.com.android.nfc=off \
+    persist.arm64.memtag.system_server=off
