crdroidandroid/android_vendor_crdroid

build: Override host metadata
* Matches the Android 12 build sandbox: https://android.googlesource.com/platform/build/+/refs/tags/android-12.0.0_r2/Changes.md

Change-Id: I032127e2724b34e649bcc73c347c8e67df1f45b0
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

build/envsetup.sh
@@ -941,3 +941,7 @@ function fixup_common_out_dir() {
         mkdir -p ${common_out_dir}
     fi
 }
+
+# Override host metadata to make builds more reproducible and avoid leaking info
+export BUILD_USERNAME=nobody
+export BUILD_HOSTNAME=android-build

Los-Ext/android_vendor_lineage

build/envsetup.sh
@@ -947,3 +947,7 @@ function fixup_common_out_dir() {
         mkdir -p ${common_out_dir}
     fi
 }
+
+# Override host metadata to make builds more reproducible and avoid leaking info
+export BUILD_USERNAME=ext
+export BUILD_HOSTNAME=android-buildbot