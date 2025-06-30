36fe4c0b0e2ec4ad1c3487897381d8028f5f2bd3
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-01 05:14:46 +0800

    common: add blur support
    Change-Id: Ie503986ef2642dbd474bfa5bee8d67688788d544

--- config/common.mk
+++ config/common.mk
@@ -172,6 +172,7 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 # Blur
 ifeq ($(TARGET_ENABLE_BLUR), true)
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.custom.blur.enable=true \
     ro.sf.blurs_are_expensive=1 \
     ro.surface_flinger.supports_background_blur=1
 else
