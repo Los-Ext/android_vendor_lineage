431378a68e02d91cccc602eb5f8d4bf88abaafc4
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-18 18:51:23 +0800

    config: boost surfaceflinger min uclamp utilization
    Change-Id: I49bfc06747a860b952c2cb9a334bde529a337b74

--- config/common_ext.mk
+++ config/common_ext.mk
@@ -10,6 +10,10 @@ endif
 
 PRODUCT_SYSTEM_PROPERTIES += ro.surface_flinger.supports_background_blur?=1
 
+# Boost surfaceflinger min uclamp utilization
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.surface_flinger.uclamp.min?=165
+
 # Cloned app exemption
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-crdroid-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-crdroid-product.xml
