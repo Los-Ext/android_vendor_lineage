72682deada459fa5189d6d7568d3d3af8bf08415
ImSpiDy <SpiDy2713@gmail.com>
2025-11-26 06:55:43 +0000

    common: Disable Debugs on userdebug builds too
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -141,7 +141,7 @@ ART_BUILD_HOST_NDEBUG := false
 ART_BUILD_HOST_DEBUG := false
 
 # Flags
-ifeq ($(TARGET_BUILD_VARIANT), user)
+ifneq ($(TARGET_BUILD_VARIANT), eng)
     # Strip the local variable table and the local variable type table to reduce
     # the size of the system image. This has no bearing on stack traces, but will
     # leave less information available via JDWP.
