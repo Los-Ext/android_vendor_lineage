c592d574b617d47bd7a8f91638388c66a4920a65
ImSpiDy <SpiDy2713@gmail.com>
2025-12-06 09:38:26 +0000

    config: Art optimizations for Low-Ram devices
    Co-authored-by: wHo-EM-i <ehteshammalik18998@gmail.com>
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- config/common_ext.mk
+++ config/common_ext.mk
@@ -38,6 +38,9 @@ PRODUCT_PACKAGES += \
     LineageSetupWizard
 endif
 
+# low end art
+LOW_RAM_ART ?= false
+
 # Memtag
 PRODUCT_PRODUCT_PROPERTIES += \
     arm64.memtag.process.system_server=off \
