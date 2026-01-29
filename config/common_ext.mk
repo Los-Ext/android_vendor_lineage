85760679663370773b729c66ea9ce3a37b754f45
Pranav Vashi <neobuddy89@gmail.com>
2026-01-29 23:20:14 +0530

    config: Fix material expressive for permission manager
    Fixes: https://github.com/crdroidandroid/issue_tracker/issues/887
    Change-Id: I1f69108f6a16dd0f80895c0bed14b50ad2476b9c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common_ext.mk
+++ config/common_ext.mk
@@ -18,6 +18,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.boot.wificountrycode?=00
 
+# Enable Material Design 3 Expressive
+PRODUCT_PRODUCT_PROPERTIES += \
+    is_expressive_design_enabled=true
+
 # Disable default frame rate limit for games
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.graphics.game_default_frame_rate.disabled=true
